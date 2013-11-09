b4337fd8061f312e1720df18aa672f14363ffc37
Fish <oonets@gmail.com>
2013-11-08 19:41:29 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Clozett::Application.config.secret_key_base = 'c462a48ede4b53d7bf8be54187ea37611e55b807ff3753e632f39aa089efd873fc047e09637f7aab78ca0b421d161e4c27257c38833813949786432547a9d398'
